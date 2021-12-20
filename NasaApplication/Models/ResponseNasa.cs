7c9db0fae00f2529fb99e89a278860b2dbeb7a22
Andrea Cicero <andrea-cicero@hotmail.it>
2021-12-20 22:28:33 +0100

    Edit ResponseNasa

--- NasaApplication/Models/ResponseNasa.cs
+++ NasaApplication/Models/ResponseNasa.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 namespace NasaApplication.Models
 {
-    public class ResponseNasa
+    public class ResponseNasa 
     {
-        internal DateTime Date;
-        internal int TemperatureC;
-        internal string Summary;
 
-        public ResponseNasa()
+        private string Title;
+        private string ContentUrl;
+        private int codeError;
+
+
+        public string Title1
         {
+            get => Title;
+            set => Title = value;
+        }
+
+        public string ContentUrl1
+        {
+            get => ContentUrl;
+            set => ContentUrl = value;
+        }
+
+        public int Codeerror
+        {
+            get => codeError;
+            set => codeError = value;
+        }
+
+        public ResponseNasa(string title, string contentUrl, int codeerror)
+        {
+            Title = title;
+            ContentUrl = contentUrl;
+            this.codeError = codeerror;
+        }
+
+        public ResponseNasa(int codeerror)
+        {
+            this.codeError = codeerror;
         }
     }
 }
+
+
